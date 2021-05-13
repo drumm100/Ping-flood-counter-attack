f43c4e4d4ebdbce6af077d594fa52d04d899d226
Marcelo Drumm <marcelo.drumm@guiainvest.com.br>
2021-05-13 20:40:54 -0300

    Add icmp check

--- ping_flood.py
+++ ping_flood.py
@@ -12,6 +12,9 @@ ETH_P_SIZE = 65536
 ETH_P_IP = 0x0800
 IP_PROTO_ICMP = 1
 
+ATTACK_TIME = 5
+KNOWN_HOSTS = {}
+
 
 def bytes_to_mac(bytes_mac):
     return ":".join("{:02x}".format(x) for x in bytes_mac)
@@ -47,10 +50,38 @@ if __name__ == "__main__":
             print("IP Packet")
             ip_header = packet[eth_length:20 + eth_length]
             iph = struct.unpack("!BBHHHBBH4s4s", ip_header)
+
+            version_ihl = iph[0]
+            version = version_ihl >> 4
+            ihl = version_ihl & 0xF
+            iph_length = ihl * 4
+            ttl = iph[5]
+
             s_addr = socket.inet_ntoa(iph[8])
             d_addr = socket.inet_ntoa(iph[9])
             print("IP Src: " + s_addr)
             print("IP Dst: " + d_addr)
+
+            protocol = iph[6]
+
+            if protocol == 1 :
+                print("ICMP Packet")
+                icmp_header = packet[iph_length + eth_length:]
+                icmph =  struct.unpack("BBHHH%ds" % (len(icmp_header) - 8), icmp_header)
+                icmp_type = icmph[0]
+                icmp_code = icmph[1]
+                icmp_id = icmph[2]
+                icmp_seq = icmph[3]
+                icmp_payload = icmph[4]
+
+                print("Type: ", icmp_type)
+                print("Code: ", icmp_code)
+
+                if icmp_type == 8 and icmp_code == 0 :
+                    print("Echo Request")
+                if icmp_type == 0 and icmp_code == 0 :
+                    print("Echo Reply")
+
 
 
 
